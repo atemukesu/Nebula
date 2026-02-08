8c41ca8b1f1401e78e7a6a694de3d6dcfbeb09b3
Atemukesu <atommix823@gmail.com>
2026-02-08 17:42:27 +0800

    feat: Implement YACL-based configuration screen integrated with ModMenu

--- src/client/java/com/atemukesu/nebula/config/NebulaYACLConfig.java
+++ src/client/java/com/atemukesu/nebula/config/NebulaYACLConfig.java
@@ -6,34 +6,40 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
 public class NebulaYACLConfig {
-    public static Screen createConfigScreen(Screen parent) {
-        ModConfig config = ModConfig.getInstance();
+        public static Screen createConfigScreen(Screen parent) {
+                ModConfig config = ModConfig.getInstance();
 
-        return YetAnotherConfigLib.createBuilder()
-                .title(Text.translatable("gui.nebula.config.title"))
-                .category(ConfigCategory.createBuilder()
-                        .name(Text.translatable("gui.nebula.config.category.general"))
-                        .option(Option.<Boolean>createBuilder()
-                                .name(Text.translatable("gui.nebula.config.render_in_game"))
-                                .description(OptionDescription
-                                        .of(Text.translatable("gui.nebula.config.render_in_game.desc")))
-                                .binding(
-                                        true,
-                                        config::shouldRenderInGame,
-                                        config::setRenderInGame)
-                                .controller(BooleanControllerBuilder::create)
-                                .build())
-                        .option(Option.<Boolean>createBuilder()
-                                .name(Text.translatable("gui.nebula.config.show_debug_hud"))
-                                .binding(
-                                        false,
-                                        config::getShowDebugHud,
-                                        config::setShowDebugHud)
-                                .controller(BooleanControllerBuilder::create)
-                                .build())
-                        .build())
-                .save(ConfigManager::saveConfig)
-                .build()
-                .generateScreen(parent);
-    }
+                return YetAnotherConfigLib.createBuilder()
+                                .title(Text.translatable("gui.nebula.config.title"))
+                                .category(ConfigCategory.createBuilder()
+                                                .name(Text.translatable("gui.nebula.config.category.general"))
+                                                .option(Option.<Boolean>createBuilder()
+                                                                .name(Text.translatable(
+                                                                                "gui.nebula.config.render_in_game"))
+                                                                .description(OptionDescription
+                                                                                .of(Text.translatable(
+                                                                                                "gui.nebula.config.render_in_game.desc")))
+                                                                .binding(
+                                                                                true,
+                                                                                config::shouldRenderInGame,
+                                                                                config::setRenderInGame)
+                                                                .controller(BooleanControllerBuilder::create)
+                                                                .build())
+                                                .option(Option.<Boolean>createBuilder()
+                                                                .name(Text.translatable(
+                                                                                "gui.nebula.config.show_debug_hud"))
+                                                                .description(OptionDescription
+                                                                                .of(Text.translatable(
+                                                                                                "gui.nebula.config.show_debug_hud.desc")))
+                                                                .binding(
+                                                                                false,
+                                                                                config::getShowDebugHud,
+                                                                                config::setShowDebugHud)
+                                                                .controller(BooleanControllerBuilder::create)
+                                                                .build())
+                                                .build())
+                                .save(ConfigManager::saveConfig)
+                                .build()
+                                .generateScreen(parent);
+        }
 }
